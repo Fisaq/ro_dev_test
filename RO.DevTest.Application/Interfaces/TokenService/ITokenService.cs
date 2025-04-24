fd6043d9b7b94946fdb428940d9906b5cf05a9b1
Fisaq <isaqueferreira15@outlook.com>
2025-04-24 13:19:32 -0300

    feat: implementacao da autenticacao com JWT e criacao da logica do LoginCommandHandler

---
+++ RO.DevTest.Application/Interfaces/TokenService/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using RO.DevTest.Domain.Entities;
+
+namespace RO.DevTest.Application.Interfaces.TokenService
+{
+    public interface ITokenService
+    {
+        string GetToken(User user, List<string> roles);
+    }
+}
